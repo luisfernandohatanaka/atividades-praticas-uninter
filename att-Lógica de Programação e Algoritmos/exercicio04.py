02a34cc00cdef8d327ca7c67bd8d002bccdcfc0e
Matheus Viscki <matheusviscki@gmail.com>
2021-08-15 22:00:50 -0300

    v1.3

--- att-Lógica de Programação e Algoritmos/exercicio04.py
+++ att-Lógica de Programação e Algoritmos/exercicio04.py
@@ -1,16 +1,26 @@
 from operator import itemgetter
 
+
+def ler_numero(mensagem):
+    while True:
+        try:
+            f = int(input(mensagem))
+            return f
+        except ValueError:
+            print('Digite um número válido')
+
+
 lista = []
 dicionario = {}
 
 while True:
     print('Digite 0 [ZERO] no código para encerrar')
-    dicionario['codigo'] = int(input('Código: '))
+    dicionario['codigo'] = ler_numero('Código: ')
     if dicionario['codigo'] == 0:
         break
     else:
-        dicionario['estoque'] = int(input('Estoque: '))
-        dicionario['minimo'] = int(input('Mínimo: '))
+        dicionario['estoque'] = ler_numero('Estoque: ')
+        dicionario['minimo'] = ler_numero('Mínimo: ')
         lista.append(dicionario.copy())
 listaOrdenada = sorted(lista, key=itemgetter('codigo'))
 
